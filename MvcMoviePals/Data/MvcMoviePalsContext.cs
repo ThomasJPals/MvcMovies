daf500d6c4b8874037e516bf39423580384825e9
ThomasJPals <thomas03.pals@gmail.com>
2021-10-11 13:16:57 +0200

    Thomas
    Gezeik

---
+++ MvcMoviePals/Data/MvcMoviePalsContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using MvcMoviePals.Models;
+
+namespace MvcMoviePals.Data
+{
+    public class MvcMoviePalsContext : DbContext
+    {
+        public MvcMoviePalsContext (DbContextOptions<MvcMoviePalsContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<MvcMoviePals.Models.Movie> Movie { get; set; }
+    }
+}
